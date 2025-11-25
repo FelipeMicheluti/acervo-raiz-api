fec6ecc0f67c914894fcc145e68ca58d6d278ac3
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:12:21 -0300

    cria admin e usuário de teste com UUIDs e timestamps

---
+++ src/database/seeders/20251123194708-demo-users.cjs
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        const passwordHash = await bcrypt.hash('senha123', 10);
+
+        await queryInterface.bulkInsert('users', [
+            {
+                id: uuidv4(),
+                name: 'Admin',
+                email: 'admin@email.com',
+                password_hash: passwordHash,
+                created_at: new Date(),
+                updated_at: new Date()
+            },
+            {
+                id: uuidv4(),
+                name: 'User Teste',
+                email: 'user@email.com',
+                password_hash: passwordHash,
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+        ]);
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert('users', null, {});
+    }
+};
