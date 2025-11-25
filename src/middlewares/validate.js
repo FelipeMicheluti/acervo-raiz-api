e508364290613627365aab31e63ed093ac63ee01
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:10:46 -0300

    implementa validação de requisiçõea com parsing e transformação

---
+++ src/middlewares/validate.js
@@ -0,0 +1,44 @@
+export const validate = (schemas = {}) => (resquest, _response, next) => {
+    try{
+       if (schemas.body) {
+      request.body = schemas.body.parse(request.body);
+    }
+
+    if (schemas.query) {
+      const validated = schemas.query.parse(request.query);
+    
+      Object.defineProperty(request, 'query', {
+        value: validated,
+        writable: true,
+        configurable: true,
+        enumerable: true
+      });
+    }
+    if (schemas.params) {
+      const validated = schemas.params.parse(request.params);
+    
+      Object.defineProperty(request, 'params', {
+        value: validated,
+        writable: true,
+        configurable: true,
+        enumerable: true
+      });
+    }
+
+        return next();
+    } catch (error) {
+
+      console.log({ error })
+    const issues = error?.issues?.map(i => ({
+      path: i.path,
+      message: i.message
+    })) ?? null;
+
+    return next({
+      message: 'Validation error',
+      status: 400,
+      code: 'BAD_REQUEST',
+      details: issues
+    });
+  }
+};
