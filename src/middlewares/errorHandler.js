731ec05c856fe5ec7f150acc81f0f72dffd9fbd7
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:11:03 -0300

    integra tratamento de HttpError e erros inesperados

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+export const errorHandler = (error, _request, response, _next) => {
+    const status = error?.status ?? 500;
+
+    const payload ={
+        message: error?.message ?? 'Internal sever error',
+        code: error?.code ?? 'INTERNAL_ERROR',
+        details: error?.detalis ?? null
+    };
+
+    return response .status(status).json(payload);
+};
