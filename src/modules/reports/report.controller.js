5f36d1b5543c1136e8cbe48ae0d2e9de75cac9de
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:07:50 -0300

    cria endpoints de create, list, get patch e remove para reports

---
+++ src/modules/reports/report.controller.js
@@ -0,0 +1,63 @@
+import { makeReportService } from "./report.service.js"
+
+export const makeReportController = () => {
+  const service = makeReportService()
+
+  const create = async (request, response, next) => {
+    try {
+      const { title, content, originLocation } = request.body;
+
+      const product = await service.create({ title, content, originLocation, createdBy: request.user.id });
+
+      return response.status(201).json(product);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  const list = async (request, response, next) => {
+    try {
+      const data = await service.list(request.query);
+      return response.json(data);
+    } catch (err) {
+      return next(err);
+    }
+  };
+
+  const get = async (request, response, next) => {
+    try {
+      return response.json(await service.get({id: request.params.id}));
+    }
+    catch (err) {
+      return next(err);
+    }
+  };
+
+  const patch = async (request, response, next) => {
+    try {
+      return response.json(await service.update({id: request.params.id, data: request.body}));
+    }
+    catch (err) {
+      return next(err);
+    }
+  };
+
+  const remove = async (request, response, next) => {
+    try {
+      await service.remove({id:request.params.id});
+      return response.status(204).send();
+    }
+    catch (err) {
+      return next(err);
+    }
+  };
+
+  return {
+    create,
+    list,
+    get,
+    patch,
+    remove
+  }
+
+}
