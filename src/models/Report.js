2bd34695e450f3d9bd363b94fa52abe559551ffd
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:09:30 -0300

    define tabela reports com timestamps e underscored

---
+++ src/models/Report.js
@@ -0,0 +1,40 @@
+import { DataTypes } from "sequelize";
+import { database } from "../config/database.js";
+
+export const Report = database.define('Report', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    originLocation: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        field: 'origin_location'
+    },
+    createdBy: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'created_by',
+        references: {
+            model: 'users',
+            key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    }
+}, {
+    tableName: 'reports',
+    timestamps: true,
+    underscored: true
+})
