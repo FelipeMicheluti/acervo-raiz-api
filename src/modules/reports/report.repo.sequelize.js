1111b2b3b30a542b6a68ba1b777179be08179b07
Felipe Micheluti <michelutifelipe@gmail.com>
2025-11-25 18:06:54 -0300

    cria reportRepoSequelize com filtros, paginação e include de usuário

---
+++ src/modules/reports/report.repo.sequelize.js
@@ -0,0 +1,66 @@
+import { Op } from "sequelize"
+import { Report } from "../../models/Report.js"
+import { User } from "../../models/User.js"
+
+export const makeReportRepoSequelize = () => {
+  return {
+    async create({ title, content, originLocation, createdBy }) {
+      const report = await Report.create({ title, content, originLocation, createdBy })
+
+      return report.toJSON()
+    },
+
+    async findById({ id }) {
+      const report = await Report.findByPk(id)
+
+      return report ? report.toJSON() : null
+    },
+
+    async findAll({ q, page = 1, limit = 10, order = 'id', dir = 'ASC' }) {
+      const where = {}
+
+      
+      if (q) {
+        where.title = { [Op.like]: `%${q}%` }; 
+    
+      }
+
+      const offset = (page - 1) * limit;
+
+      const { count, rows } = await Report.findAndCountAll({
+        where,
+        include: [{
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name']
+        }],
+        limit,
+        offset,
+        order: [[order, dir]]
+      });
+
+      return {
+        items: rows.map(p => p.toJSON()),
+        page,
+        limit,
+        total: count,
+        totalPages: Math.ceil(count / limit)
+      };
+    },
+  async update({ id, data }) {
+      const report = await Report.findByPk(id)
+      if (!report) return null
+
+      await report.update(data)
+      return report.toJSON()
+    },
+
+    async delete({ id }) {
+      const report = await Report.findByPk(id)
+      if (!report) return false
+
+      await report.destroy()
+      return true
+    }
+  }
+}
